5a0192d671e5ab4b9533b992110c49810e3def51
nils3105102 <daniil.pron4enkov@gmail.com>
2019-08-08 18:41:44 +0300

    first part: auth

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <h3>Your listings</h3>
+                    @if(count($listings))
+                    <table>
+                        <tr>
+                            <th>Company</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                        @foreach($listings as $listing)
+                        <tr>
+                            <td>{{$listing->name}}</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        @endforeach
+                    </table>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
